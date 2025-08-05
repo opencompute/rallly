c18b77cb511b2a4ea382b4db4b088b9fa1716ac1
Sean Chen <xuchenx@gmail.com>
2025-08-04 21:44:38 -0700

    update logo

--- apps/web/src/trpc/routers/polls.ts
+++ apps/web/src/trpc/routers/polls.ts
@@ -862,7 +862,7 @@ export const polls = router({
       }
 
       const eventId = nanoid();
-      const uid = `${eventId}@rallly.co`;
+      const uid = `${eventId}@kinpal.com`;
 
       const attendees = poll.participants.filter((p) =>
         p.votes.some((v) => v.optionId === input.optionId && v.type !== "no"),
@@ -900,7 +900,7 @@ export const polls = router({
         const event = await tx.scheduledEvent.create({
           data: {
             id: eventId,
-            uid: `${eventId}@rallly.co`,
+            uid: `${eventId}@kinpal.com`,
             start: eventStart.toDate(),
             end:
               option.duration > 0
